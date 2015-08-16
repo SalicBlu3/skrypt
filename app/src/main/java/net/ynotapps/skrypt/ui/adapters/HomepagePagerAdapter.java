9f38c1b6773dfa4183c72d6d7fdbd82b7031d20e
SalicBlu3 <dougy_style@hotmail.com>
2015-08-16 11:26:33 +1000

    Added Page Tabs

--- app/src/main/java/net/ynotapps/skrypt/ui/adapters/HomepagePagerAdapter.java
+++ app/src/main/java/net/ynotapps/skrypt/ui/adapters/HomepagePagerAdapter.java
@@ -9,14 +9,31 @@ import net.ynotapps.skrypt.ui.fragments.SkryptFragment;
 public class HomepagePagerAdapter extends FragmentStatePagerAdapter {
 
     public static final int FRAGMENT_COUNT = 2;
+    public static final String FLOW_TITLE = "Work on your Flow";
 
     public HomepagePagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return FLOW_TITLE;
+            default:
+                return "Something Else";
+        }
+    }
+
+    @Override
     public Fragment getItem(int position) {
-        return new SkryptFragment();
+
+        switch (position) {
+            case 0:
+                return new SkryptFragment();
+            default:
+                return new Fragment();
+        }
     }
 
     @Override
